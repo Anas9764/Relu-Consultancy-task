966bf83592660cbdc666f7bd58eed0fdac9a820a
Anas9764 <anasqureshi9764@gmail.com>
2023-10-29 19:19:51 +0530

    Login and signup page crated

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './Login.module.css'
+import {Link} from "react-router-dom"
+import InputControl from '../InputControl/InputControl';
 
 
 const Login = () => {
@@ -7,6 +9,20 @@ const Login = () => {
     <div className={styles.container}>
     <div className={styles.innerBox}>
       <h1 className={styles.heading}>Login</h1>
+
+      <InputControl label="Email" placeholder="Enter Email address"/>
+      <InputControl label="Password" placeholder="Enter Your Password"/>
+      
+<div className={styles.footer}>
+<button>Login</button>
+<p>
+  Aleady have an account?{""}
+  <span>
+    <Link to="/signup">Sign Up</Link>
+  </span>
+</p>
+
+</div>
       </div>
       </div>
   );
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './Signup.module.css'
+import {Link} from "react-router-dom"
+import InputControl from '../InputControl/InputControl';
+
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
+    <div className={styles.container}>
+    <div className={styles.innerBox}>
+      <h1 className={styles.heading}>Signup</h1>
+
+      <InputControl label="Name" placeholder="Enter Your Name"/>
+      <InputControl label="Email" placeholder="Enter Email address"/>
+      <InputControl label="Password" placeholder="Enter Your Password"/>
+      
+<div className={styles.footer}>
+<button>Signup</button>
+<p>
+  Aleady have an account?{""}
+  <span>
+    <Link to="/Login">Login</Link>
+  </span>
+</p>
+
+</div>
+      </div>
+      </div>
+  );
 }
 
 export default Signup
